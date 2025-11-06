9801d4db5f4187b23808866036756977b18bac1d
Jure Rot <jure.rot96@gmail.com>
2025-11-06 20:49:12 +0100

    updated wallpaper scripts and settings for laptop

--- scripts/scripts/generate_random_background.sh
+++ scripts/scripts/generate_random_background.sh
@@ -6,7 +6,7 @@ b=$(($RANDOM % 255))
 solid_color="rgb($r, $g, $b)"
 
 rm -f $HOME/Images/backgrounds/backgroundDP1.png
-magick -size 1920x1080 canvas:"$solid_color" $HOME/Images/backgrounds/backgroundDP1.png
+magick -size 1366x768 canvas:"$solid_color" $HOME/Images/backgrounds/backgroundeDP1.png
 
-rm -f $HOME/Images/backgrounds/backgroundDP2.png
-magick -size 1080x1920 canvas:"$solid_color" $HOME/Images/backgrounds/backgroundDP2.png
+#rm -f $HOME/Images/backgrounds/backgroundDP2.png
+#magick -size 1080x1920 canvas:"$solid_color" $HOME/Images/backgrounds/backgroundDP2.png
