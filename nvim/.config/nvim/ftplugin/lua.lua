c317be254cba66bac74ef05cc83b542bb7a3829b
Jure Rot <jure.rot96@gmail.com>
2025-12-12 20:13:44 +0100

    added luajit keybinding to nvim

--- nvim/.config/nvim/ftplugin/lua.lua
+++ nvim/.config/nvim/ftplugin/lua.lua
@@ -1,3 +1,5 @@
 local opts = { noremap = true, silent = true }
 
 vim.keymap.set("n", "<leader>pr", ":w<CR>:!lua %<CR>", opts) -- save and run current file with lua cli tool
+
+vim.keymap.set("n", "<leader>pfr", ":w<CR>:!luajit %<CR>", opts) -- save and run current file with luaJIT cli tool
